d80cd41d1b682d1f044852ca20e6cd5ca76a56b8
Jason <jason.mathias@gmail.com>
2018-07-07 18:44:40 -0400

    Added GM, movement and Fire1 stop

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MovingCube.CurrentCube.Stop();
+        }
+	}
+}
--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingCube : MonoBehaviour {
+    public static MovingCube CurrentCube { get; private set; }
+    private bool isMoving = true;
+
+    [SerializeField]
     private float moveSpeed = 1;
+
+    private void Awake()
+    {
+        //TODO: Move to Spawner
+        CurrentCube = this;
+    }
+
+    internal void Stop()
+    {
+        isMoving = false;
+    }
     
 	void Update () {
+        if(isMoving)
         transform.position += transform.forward * Time.deltaTime * moveSpeed;
 
 	}
