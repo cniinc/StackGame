4695b542cf3e558873991d9115bb22b4aa7a5e3a
Jason <jason.mathias@gmail.com>
2018-07-07 18:13:29 -0400

    Hello World

---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour {
+    private float moveSpeed = 1;
+    
+	void Update () {
+        transform.position += transform.forward * Time.deltaTime * moveSpeed;
+
+	}
+}
