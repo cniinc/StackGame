61b81b2e68fdc868b64bec339c91bf0ec2120ca3
Jason <jason.mathias@gmail.com>
2018-07-07 20:26:51 -0400

    Added spawner without spawn functionality.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    private int currentSpawner = 0;
+    [SerializeField]
+    private List<Spawner> Spawners;
+    [SerializeField]
+    private Cube startCube;
+
+    
+    public static Cube LastCube;
+
+    private void Awake()
+    {
+        LastCube = startCube;
+    }
 
     void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
             MovingCube.CurrentCube.Stop();
         }
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            print("test spawn");
+        }
 	}
+
+
+
+
+
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Spawner : MonoBehaviour {
+    [SerializeField]
+    private MovingCube movingCube;
+
+
+    public void Spawn()
+    {
+        print("Spawn");
+        Instantiate(movingCube);
+    }
+}
